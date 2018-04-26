77c54e53d1c6d63ebddcc3c7bb4c5582013ef722
DuckSoft <ducksoft@foxmail.com>
2018-04-26 10:40:29 +0800

    Fixing Line Breaks

--- setting.py
+++ setting.py
@@ -1,1 +1,24 @@
-"""Project Configurations for Flask Application* You may use any of them in your application by using `app.config[]`"""# DevelopmentDEBUG = TrueLISTEN_HOST_DEV = r'127.0.0.1'LISTEN_PORT_DEV = 5000# ProductionLISTEN_HOST_PRODUCTION = r'0.0.0.0'LISTEN_PORT_PRODUCTION = 8080# SessionSECRET_KEY = r'xsa2Df323rUF9*EWFH&*h(dsa#Y(#&HDWDH(/,?RT'# SQLAlchemySQLALCHEMY_TRACK_MODIFICATIONS = FalseSQLALCHEMY_DATABASE_URI = r"mysql://root:rootpassword@localhost:3306/database?charset=utf8mb4"# ...You may add more!
+"""
+Project Configurations for Flask Application
+
+* You may use any of them in your application by using `app.config[]`
+"""
+
+# Development
+DEBUG = True
+LISTEN_HOST_DEV = r'127.0.0.1'
+LISTEN_PORT_DEV = 5000
+
+# Production
+LISTEN_HOST_PRODUCTION = r'0.0.0.0'
+LISTEN_PORT_PRODUCTION = 8080
+
+# Session
+SECRET_KEY = r'xsa2Df323rUF9*EWFH&*h(dsa#Y(#&HDWDH(/,?RT'
+
+# SQLAlchemy
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+SQLALCHEMY_DATABASE_URI = r"mysql://root:rootpassword@localhost:3306/database?charset=utf8mb4"
+
+# ...You may add more!
+
